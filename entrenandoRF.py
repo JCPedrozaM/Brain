5a82dd66becece6baacde99a761fd303987730ed
danymtzu <32463081+danymtzu@users.noreply.github.com>
2020-11-04 12:20:51 -0700

    Cambio de la ruta Data en entrenandoRF

--- entrenandoRF.py
+++ entrenandoRF.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import numpy as np
 
-dataPath = 'Data' #Cambia a la ruta donde hayas almacenado Data
+dataPath = 'Brain/Data' #Cambia a la ruta donde hayas almacenado Data
 peopleList = os.listdir(dataPath)
 print('Lista de personas: ', peopleList)
 
