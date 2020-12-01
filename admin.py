96923dde47a8cc8fc51c5f23c2d96de8aa3a5c16
danymtzu <32463081+danymtzu@users.noreply.github.com>
2020-12-01 13:26:57 -0700

    Documentación menu y admin

--- admin.py
+++ admin.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-#import ReconocimientoFacial
 import os
 import sys
+
+#VENTANA DE PANEL DE ADMINISTRACIÓN
 
 window=tk.Tk()
 
@@ -17,15 +18,16 @@ frame=tk.Frame(window,width=450, height=70)
 frame.pack()
 frame.config(bg="#93D8FF")
 
-
+#Etiqueta de título de la ventana
 label= tk.Label(frame, text="Panel de Administración",font=("Century Gothic", 16), bg="#93D8FF")
 label.place(x=90, y=20)
 
-#Creación Frame 2
+#Creación Frame 2 para poner los botones
 frame2=tk.Frame(window,width=450, height=300)
 frame2.pack()
 frame2.config(bg="#FFFFFF")
 
+#Métodos para los botones
 def captRostrosCon():
     os.system('python capturandoRostrosCon.py')
 
@@ -42,22 +44,27 @@ def atras():
 def instalarRequerimientos():
     os.system('pip install -r requirements.txt')
 
+#Botón Captura rostro con cubrebocas
 con_btn= tk.PhotoImage(file="Botones/conbtn.png")
 con_lbl= tk.Label(image=con_btn)
 btn1= tk.Button(frame2, image=con_btn , command=captRostrosCon, bg="#FFFFFF", borderwidth=0).place(x=20, y=30)
 
+#Botón Captura rostro sin cubrebocas
 sin_btn= tk.PhotoImage(file="Botones/sinbtn.png")
 sin_lbl= tk.Label(image=sin_btn)
 btn1= tk.Button(frame2, image=sin_btn , command=captRostrosSin, bg="#FFFFFF", borderwidth=0).place(x=140, y=120)
 
+#Botón Ejecutar entrenamiento
 train_btn= tk.PhotoImage(file="Botones/entrenarbtn.png")
 train_lbl= tk.Label(image=train_btn)
 btn3= tk.Button(frame2, image=train_btn, command=entrenar, bg="#FFFFFF", borderwidth=0).place(x=250, y=200)
 
+#Botón Regresar
 back_btn= tk.PhotoImage(file="Botones/btnhome.png")
 back_lbl= tk.Label(image=back_btn)
 btn4= tk.Button(frame2, image=back_btn, command=atras, bg="#FFFFFF", borderwidth=0).place(x=40, y=240)
- 
+
+#Botón Instalar requerimientos
 install_btn= tk.PhotoImage(file="Botones/btnrequerimientos.png")
 install_lbl= tk.Label(image=install_btn)
 btn5= tk.Button(frame2, image=install_btn, command=instalarRequerimientos, bg="#FFFFFF", borderwidth=0).place(x=320, y=30)
--- menu.py
+++ menu.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import os
 import sys
 
+#VENTANA PRINCIPAL
+
 window=tk.Tk()
 
 window.title("Detección de Cubrebocas") #Título de la ventana
@@ -16,15 +18,16 @@ frame=tk.Frame(window,width=450, height=70)
 frame.pack()
 frame.config(bg="#93D8FF")
 
-
+#Etiqueta de título de la ventana
 label= tk.Label(frame, text="Detección de cubrebocas",font=("Century Gothic", 16), bg="#93D8FF")
 label.place(x=90, y=20)
 
-#Creación Frame 2
+#Creación Frame 2 para colocar botones
 frame2=tk.Frame(window,width=450, height=200)
 frame2.pack()
 frame2.config(bg="#FFFFFF")
 
+#Métodos para los botones
 def recFacial():
     os.system('python ReconocimientoFacial.py')
 
@@ -32,11 +35,12 @@ def openAdmin():
     window.destroy()
     os.system('python admin.py')
 
-
+#Botón comenzar detección
 start_btn= tk.PhotoImage(file="Botones/btncomenzar.png")
 start_lbl= tk.Label(image=start_btn)
 my_btn= tk.Button(frame2, image=start_btn, command=recFacial, borderwidth=0, bg="#FFFFFF").place(x=150, y=20)
 
+#Botón para ir al Panel de Administración
 admin_btn= tk.PhotoImage(file="Botones/btnadmin.png")
 admin_lbl= tk.Label(image=admin_btn)
 my_btn2= tk.Button(frame2, image=admin_btn, command=openAdmin, borderwidth=0, bg="#FFFFFF").place(x=335, y=145)
