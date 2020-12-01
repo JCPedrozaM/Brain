6c3225c669d1eb44bb0ec084fb594a60903af233
danymtzu <32463081+danymtzu@users.noreply.github.com>
2020-12-01 13:17:16 -0700

    Documetación de capRostrosSin y Con

--- capturandoRostrosSin.py
+++ capturandoRostrosSin.py
@@ -3,21 +3,25 @@ import os
 import imutils
 from datetime import datetime
 
+#PROGRAMA QUE CAPTURA ROSTROS SIN CUBREBOCAS
 
-personName = "Sin cubrebocas"
-dataPath = "Data" #Cambia a la ruta donde hayas almacenado Data
+personName = "Sin cubrebocas"#Nombre de la etiqueta
+dataPath = "Data" #Carpeta en donde se almacenan las imagenes capturadas
 personPath = dataPath + "/" + personName
 
+#Se crea la carpeta de la etiqueta en caso de que no exista
 if not os.path.exists(personPath):
 	print("Carpeta creada: ",personPath)
 	os.makedirs(personPath)
 
+#Se activa la cámara web
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-#cap = cv2.VideoCapture('Video.mp4')
 
+#Método de OpenCV para la detección de objetos en video
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
 count = 0
 
+#Detección de rostro
 while True:
 
 	ret, frame = cap.read()
@@ -27,7 +31,8 @@ while True:
 	auxFrame = frame.copy()
 
 	faces = faceClassif.detectMultiScale(gray,1.3,5)
-
+	
+#Almacenamiento de imagenes en la ruta correspondiente
 	for (x,y,w,h) in faces:
 		cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
 		rostro = auxFrame[y:y+h,x:x+w]
